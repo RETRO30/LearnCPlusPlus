6b9a11a04d7f8319df92e302772409ecdb6abf9c
RETRO30 <ssh-off@ya.ru>
2023-08-30 21:57:37 +0200

    add calculator

---
+++ Solves/Calculator/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+class Symbol {
+
+};
+
+class Expression {
+    public:
+        std::string string_exp;
+
+        string string_exp;
+
+        double getResult() {
+            double res = 1.0;
+            Symbol symbols = getSymbols();  
+            return res;   
+        }
+
+        char getTree() {
+            return " ";
+        }
+
+        Symbol getSymbols() {
+            return ''
+        }
+
+};
