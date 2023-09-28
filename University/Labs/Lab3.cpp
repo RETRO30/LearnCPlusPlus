fc0574de3f570e66b733c17f17b8fd4fb4874b79
RETRO30 <ssh-off@ya.ru>
2023-09-28 15:45:35 +0200

    add lecture and finish lab 3

---
+++ University/Labs/Lab3.cpp
@@ -0,0 +1,50 @@
+ #include <iostream>
+
+ int main(){
+
+    // Пункт 1
+    int A, B, C;
+    std::cin >> A >> B >> C;
+    if (A < B && B < C){
+        std::cout << "A < B < C -> " << C << B << A << std::endl; 
+    }
+    else if (A % C == 0){
+        std::cout << "A%C==0 -> " << A/C + B << std::endl;
+    }
+    else {
+        std::cout << "Doesn't fit any of the conditions -> " << A + B + C << std::endl;
+    }
+
+    // Пункт 2
+    int N;
+    std::cin >> N;
+    N = N < 50 && N > 0 ? N:-1;
+    switch (N)
+    {
+    case (1):
+        std::cout << "Is the square" << std::endl;
+        break;
+    case (4):
+        std::cout << "Is the square" << std::endl;
+        break;
+    case (9):
+        std::cout << "Is the square" << std::endl;
+        break;
+    case (16):
+            std::cout << "Is the square" << std::endl;
+            break;
+    case (25):
+        std::cout << "Is the square" << std::endl;
+        break;
+    case (36):
+        std::cout << "Is the square" << std::endl;
+        break;
+    case (49):
+        std::cout << "Is the square" << std::endl;
+        break;
+    default:
+        std::cout << "Is not a square" << std::endl;
+        break;
+    }
+
+ }
--- University/Lecture3.cpp
+++ University/Lecture3.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <iomanip>
+#define N_Max 10
 
 int main() {
+    // Массивы
+
+    const int N = 10;
+
+    int mas[N] = {1, 3, 5, 6, 9, 4, 30, 20, 2};
+
+    int n = 9;
+    // Сортировка обменами
+
+    for (int i=0; i< n-1; i++ ){
+        for (int j=i+1; j<n; j++){
+            if (mas[i]%10 < mas[j]%10){
+                continue;
+            }
+            else {
+                std::swap(mas[i], mas[j]);
+            }
+        }
+    }
+
+    for (int i=0; i < n; i++){
+        std::cout << mas[i] << std::endl;
+    }
+
+    // std::cout << sizeof(mas)/sizeof(mas[0]) << std::endl;
+    // std::cout << std::size(mas) << std::endl;
     
 
 
